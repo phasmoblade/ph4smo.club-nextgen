405913ad6d1ebac6838f0349774162805888b7bc
phasmoblade <phasmoblade@gmail.com>
2026-04-13 12:08:04 +0300

    v1.0.3
    Added FTAP
    Added Ninja Legends

--- loader.lua
+++ loader.lua
@@ -4,7 +4,9 @@ local SupportedGames = {
     [4746041618] = "https://raw.githubusercontent.com/phasmoblade/ph4smo.club-nextgen/refs/heads/main/steel-titans.lua",
     [6516141723] = "https://raw.githubusercontent.com/phasmoblade/ph4smo.club-nextgen/refs/heads/main/doors.lua",
     [79305036070450] = "https://raw.githubusercontent.com/phasmoblade/ph4smo.club-nextgen/refs/heads/main/spin-a-baddie.lua",
-    [70845479499574] = "https://raw.githubusercontent.com/phasmoblade/ph4smo.club-nextgen/refs/heads/main/bite-by-night.lua"
+    [70845479499574] = "https://raw.githubusercontent.com/phasmoblade/ph4smo.club-nextgen/refs/heads/main/bite-by-night.lua",
+    [6961824067] = "https://raw.githubusercontent.com/phasmoblade/ph4smo.club-nextgen/refs/heads/main/ftap.lua",
+    [3956818381] = "https://raw.githubusercontent.com/phasmoblade/ph4smo.club-nextgen/refs/heads/main/ninja-legends.lua"
 }
 
 if SupportedGames[PlaceId] then
