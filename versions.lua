eacfe3906daff3a5782ec5f8a8c752786b819d0a
phasmoblade <phasmoblade@gmail.com>
2026-04-18 17:38:13 +0300

    18.04.2026

---
+++ versions.lua
@@ -0,0 +1,9 @@
+return {
+	-- ["arsenal"] = "1.0",
+	["bite-by-night"] = "1.4",
+	["doors"] = "1.1",
+	["ftap"] = "1.0",
+	["ninja-legends"] = "1.0",
+	["spin-a-baddie"] = "1.0.2",
+	["steel-titans"] = "1.1"
+}
