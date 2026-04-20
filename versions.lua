dd05d60f2eb07992916d102c929d90dcab080730
phasmoblade <phasmoblade@gmail.com>
2026-04-20 22:27:20 +0300

    upd

--- versions.lua
+++ versions.lua
@@ -2,8 +2,9 @@ return {
 	["bite-by-night"] = "1.6",
 	["doors"] = "1.1",
 	["ftap"] = "1.0",
+	["flick"] = "1.0"
 	["ninja-legends"] = "1.0",
+	["project-delta"] = "1.0",
 	["spin-a-baddie"] = "1.0.2",
 	["steel-titans"] = "1.1",
-	["flick"] = "1.0"
 }
