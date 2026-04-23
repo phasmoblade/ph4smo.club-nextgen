fcbb530639b2fd918ebbb512bb3fb236047dbbf6
phasmoblade <phasmoblade@gmail.com>
2026-04-23 10:38:13 +0300

    /

--- versions.lua
+++ versions.lua
@@ -1,10 +1,11 @@
 return {
-	["bite-by-night"] = "1.6",
+	["bite-by-night"] = "1.7",
 	["doors"] = "1.1",
-	["flick"] = "1.0",
 	["ftap"] = "1.0",
+	["flick"] = "refreshed",
+	["main-script"] = "0.0",
 	["ninja-legends"] = "1.0",
-	["project-delta"] = "1.0",
+	["project-delta"] = "1.1",
 	["spin-a-baddie"] = "1.0.2",
 	["steel-titans"] = "1.1",
 }
